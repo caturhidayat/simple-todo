123412e2b6321b3e21a4c3e6f19796651ded59fd
Catur Hidayat <caturhidayat00@gmail.com>
2024-03-29 00:19:39 +0700

    fix(app): fix string database and remove .env

--- app/db.ts
+++ app/db.ts
@@ -1,7 +1,7 @@
 // db.js
 import postgres from 'postgres'
 
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString || '')
+const connectionString = process.env.DATABASE_URL || '' // Provide a default value for connectionString
+const sql = postgres(connectionString)
 
 export default sql
